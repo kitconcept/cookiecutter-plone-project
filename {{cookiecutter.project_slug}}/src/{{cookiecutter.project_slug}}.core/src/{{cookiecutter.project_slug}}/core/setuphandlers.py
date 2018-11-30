c4cf4d4781f75a2cc5fed7e4c967497d144f97ab
Timo Stollenwerk <tisto@plone.org>
2018-11-30 06:30:42 +0100

    Make setuphandlers.py work.

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.core/src/{{cookiecutter.project_slug}}/core/setuphandlers.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.core/src/{{cookiecutter.project_slug}}/core/setuphandlers.py
@@ -20,7 +20,6 @@ from zope.component.interfaces import IFactory
 from zope.container.interfaces import INameChooser
 from zope.interface import implementer
 from zope.interface import alsoProvides
-from .interfaces import ISpecificKontaktViewMarker
 from plone.dexterity.utils import createContentInContainer
 
 import os
@@ -46,7 +45,7 @@ def create_content(portal, content):
             relative_path = obj.get('path').rstrip('/')
             relative_path = relative_path.split('/')[1:]
             container = portal.restrictedTraverse(relative_path)
-        except: # noqa
+        except:  # noqa
             # TODO: handle
             pass
 
@@ -99,7 +98,7 @@ def create_content(portal, content):
             new.header_caption_en = obj.get('header_caption_en')
 
         # collection
-        from plone.app.contenttypes.behaviors.collection import ICollection as ICollection_behavior # noqa
+        from plone.app.contenttypes.behaviors.collection import ICollection as ICollection_behavior  # noqa
         if obj.get('type') == 'Collection' and obj.get('query'):
             wrapped = ICollection_behavior(new)
             wrapped.query = obj.get('query')
@@ -134,7 +133,7 @@ def create_content(portal, content):
         if obj.get('type') == 'FormSelectionField' and obj.get('fgVocabulary'):
             new.fgVocabulary = obj.get('fgVocabulary')
 
-        if obj.get('type') == 'FormMultiSelectionField' and obj.get('fgVocabulary'): # noqa
+        if obj.get('type') == 'FormMultiSelectionField' and obj.get('fgVocabulary'):  # noqa
             new.fgVocabulary = obj.get('fgVocabulary')
 
         # exclude from navigation
@@ -188,13 +187,13 @@ def post_install(context):
     portal = api.portal.get()
 
     # Rename 'News Item' -> 'Aktuelles'
-    change_content_type_title(portal, 'News Item', 'Meldung')
+    # change_content_type_title(portal, 'News Item', 'Meldung')
 
     # indexes (dropdown navigation / leadimage)
-    wanted_indexes = (
-        ('exclude_from_nav', 'BooleanIndex'),
-    )
-    add_catalog_indexes(context, wanted=wanted_indexes)
+    # wanted_indexes = (
+    #     ('exclude_from_nav', 'BooleanIndex'),
+    # )
+    # add_catalog_indexes(context, wanted=wanted_indexes)
 
     # Remove front-page from nav
     if 'front-page' in portal:
@@ -208,7 +207,7 @@ def import_content(context):
     portal = api.portal.get()
 
     # run only on fresh portals
-    if 'vereinigungen' in portal.objectIds():
+    if 'news' in portal.objectIds():
         return
 
     # users
@@ -221,51 +220,10 @@ def import_content(context):
         properties=None
     )
 
-    api.user.create(
-        email='wobst@dpg-physik.de',
-        username='wobst',
-        password='welcome',
-        roles=('Site Administrator', ),
-        properties=None
-    )
-
-    api.user.create(
-        email='rutowski@dpg-physik.de',
-        username='rutowski',
-        password='welcome',
-        roles=('Site Administrator', ),
-        properties=None
-    )
-
-    users = [
-        u'Arias',
-        u'Boettcher',
-        u'Brodesser',
-        u'Carstensen',
-        u'Degenhardt',
-        u'Derichs',
-        u'Dohrmann',
-        u'Duechs',
-        u'Genath',
-        u'Godau',
-        u'Hahn',
-        u'Hensel',
-        u'Hundsdoerfer',
-        u'Labedzke',
-        u'Lemmer',
-        u'Metzelthin',
-        u'Nunner',
-        u'Oss',
-        u'Samulat',
-        u'Schaar',
-        u'Schulz',
-        u'Stawinoga',
-        u'Wensing',
-        u'Zoll',
-    ]
+    users = []
     for user in users:
         api.user.create(
-            email='no-reply@dpg-physik.de',
+            email='no-reply@example.com',
             username=user,
             password='welcome',
             roles=('Site Administrator', ),
@@ -287,48 +245,9 @@ def import_content(context):
     create_content(portal, content)
 
     # Portlets at Vereinigung
-    setupPortletAt(portal, 'portlets.Events', 'plone.rightcolumn', '/vereinigungen/fachliche-vereinigungen/sektion-kondensierte-materie/halbleiterphysik') # noqa
-    setupPortletAt(portal, 'portlets.Events', 'plone.rightcolumn', '/vereinigungen/fachliche-vereinigungen/fachliche-vereinigungen/sektion-materie-und-kosmos/gravitation-und-relativitaetstheorie') # noqa
-    setupNavigationPortlet(portal['vereinigungen']['fachliche-vereinigungen']['sektion-materie-und-kosmos']['gravitation-und-relativitaetstheorie']) # noqa
-
-    events = [
-        {
-            'title': u'EventTitle',
-            'description': u'A couple of lines of text may go here. Not too much, just enough to draw some attention.', # noqa
-            'location': u'Berlin',
-            'subjects': ['Exkursion'],
-        },
-    ]
-    tz = pytz.timezone('Europe/Vienna')
-    for idx, event in enumerate(events):
-        _id = 'event-{}'.format(idx)
-        api.content.create(
-            type='Event',
-            id=_id,
-            title=event.get('title'),
-            description=event.get('description'),
-            container=kalender
-        )
-        api.content.transition(
-            obj=portal.veranstaltungen[_id],
-            to_state='published'
-        )
-        subjects = event.get('subjects')
-        event = IEventBasic(kalender[_id])
-
-        if subjects:
-            event.setSubject(subjects)
-
-        if idx < 3:
-            event.start = tz.localize(datetime.now() + timedelta(days=1))
-            event.end = tz.localize(datetime.now() + timedelta(days=1, hours=1)) # noqa
-        elif idx >= 3 and idx < 6:
-            event.start = tz.localize(datetime.now() + timedelta(days=(idx-2)*30)) # noqa
-            event.end = tz.localize(datetime.now() + timedelta(days=(idx-2)*30, hours=1)) # noqa
-        else:
-            event.start = tz.localize(datetime.now() - timedelta(days=(idx-2)*30)) # noqa
-            event.end = tz.localize(datetime.now() - timedelta(days=(idx-2)*30, hours=1)) # noqa
-        event.reindexObject()
+    # setupPortletAt(portal, 'portlets.Events', 'plone.rightcolumn', '/vereinigungen/fachliche-vereinigungen/sektion-kondensierte-materie/halbleiterphysik') # noqa
+    # setupPortletAt(portal, 'portlets.Events', 'plone.rightcolumn', '/vereinigungen/fachliche-vereinigungen/fachliche-vereinigungen/sektion-materie-und-kosmos/gravitation-und-relativitaetstheorie') # noqa
+    # setupNavigationPortlet(portal['vereinigungen']['fachliche-vereinigungen']['sektion-materie-und-kosmos']['gravitation-und-relativitaetstheorie']) # noqa
 
     # Delete Plone content
 
@@ -342,93 +261,9 @@ def import_content(context):
     #     api.content.delete(obj=portal['front-page'])
 
     setupNavigationPortlet(portal['beispiele']['seite'])
-    setupDPGLogin(portal)
-
-    for index in range(5):
-        api.content.create(
-            type='Image',
-            title='Image-{}'.format(index),
-            image=NamedBlobImage(open(GALLERY_IMAGE, 'rb').read(), filename=u'image.jpg'), # noqa
-            license='This is the image license',
-            container=portal['beispiele']['seite']
-        )
 
     # Set permissions for Intranet folder
-    portal['interner-bereich'].manage_setLocalRoles('Authenticated Users', ['Reader', ]) # noqa
-
-    obj = api.content.create(
-        type='Calendar',
-        id='calendar',
-        title='DPG Calendar',
-        container=portal['veranstaltungen']
-    )
-    api.content.transition(
-        obj=obj,
-        to_state='published'
-    )
-
-    # Mark Physikzentrum Bad Honnef and Magnus-Haus Berlin unsere standorte
-    alsoProvides(portal['magnus-haus-berlin'], ISpecificKontaktViewMarker) # noqa
-    alsoProvides(portal['physikzentrum-bad-honnef'], ISpecificKontaktViewMarker) # noqa
-
-    portal['magnus-haus-berlin'].exclude_from_nav = True
-    portal['physikzentrum-bad-honnef'].exclude_from_nav = True
-
-    portal['magnus-haus-berlin'].reindexObject()
-    portal['physikzentrum-bad-honnef'].reindexObject()
-
-    if 'front-page' in portal:
-        api.content.delete(portal['front-page'])
-
-    # Create Homepage
-    homepage = createContentInContainer(
-        portal, "Homepage", id='front-page', title='Homepage', checkConstraints=False, # noqa
-        slider_image1=NamedBlobImage(open(SLIDERIMG1, 'rb').read(), filename=u'slider1.jpg'),  # noqa
-        slider_title1=u'DPG-Frühjahrstagungen 2019',
-        slider_text1=u'Ab sofort ist die Anmeldung zu den DPG-Frühjahrstagungen in Rostock, München, Aachen und Regensburg möglich.', # noqa
-        slider_url1='/aktivitaten-und-programme/tagungen/dpg-fruehjahrstagungen', # noqa
-        slider_image2=NamedBlobImage(open(SLIDERIMG2, 'rb').read(), filename=u'slider2.jpg'),  # noqa
-        slider_title2=u'Jetzt DPG-Mitglied werden',
-        slider_text2=u'Vernetzung, wissenschaftlicher Austausch, persönlicher Kontakt und viele Fördermöglichkeiten – das bietet Ihnen die DPG!', # noqa
-        slider_url2='/ueber-uns/mitgliedschaft',
-        slider_image3=NamedBlobImage(open(SLIDERIMG3, 'rb').read(), filename=u'slider3.jpg'),  # noqa
-        slider_title3=u'Erste DPG-Herbsttagung 2019',
-        slider_text3=u'Mit der ersten DPG-Herbsttagung beginnt ein neues Tagungsformat, das je ein Thema in den Fokus nimmt.', # noqa
-        slider_url3='/aktivitaten-und-programme/tagungen',
-        slider_image4=NamedBlobImage(open(SLIDERIMG4, 'rb').read(), filename=u'slider4.jpg'),  # noqa
-        slider_title4=u'Aktiv sein in Ihrer DPG!',
-        slider_text4=u'Viele ehrenamtlich tätige Mitglieder und ihr Engagement machen die DPG sehr vielfältig – gestalten Sie aktiv mit!', # noqa
-        slider_url4='/aktivitaten-und-programme/selbst-aktiv-werden',
-        text=RichTextValue(
-u"""
-<h1>Die Deutsche Physikalische Gesellschaft e. V.</h1>
-<p class="intro">Die DPG ist die älteste nationale und weltweit größte physikalische Fachgesellschaft. Als gemeinn&uuml;tziger Verein verfolgen wir keine wirtschaftlichen Interessen, sondern dienen ausschließlich der Physik. Wir fördern mit Tagungen, Veranstaltungen und Publikationen den Wissenstransfer innerhalb der wissenschaftlichen Gemeinschaft und möchten allen Neugierigen ein Fenster zur Physik öffnen. Unsere Schwerpunkte sind die Förderung des naturwissenschaftlichen Nachwuchses und der Chancengleichheit.</p>
-""" # noqa
-            'text/html',
-            'text/html'
-        ),
-        text_en=RichTextValue(
-u"""
-<h1>ENG Die Deutsche Physikalische Gesellschaft e. V.</h1>
-<p class="intro">ENG Die DPG ist die älteste nationale und weltweit größte physikalische Fachgesellschaft. Als gemeinn&uuml;tziger Verein verfolgen wir keine wirtschaftlichen Interessen, sondern dienen ausschließlich der Physik. Wir fördern mit Tagungen, Veranstaltungen und Publikationen den Wissenstransfer innerhalb der wissenschaftlichen Gemeinschaft und möchten allen Neugierigen ein Fenster zur Physik öffnen. Unsere Schwerpunkte sind die Förderung des naturwissenschaftlichen Nachwuchses und der Chancengleichheit.</p>
-""" # noqa
-            'text/html',
-            'text/html'
-        )
-    )
-    homepage.exclude_from_nav = True
-    portal.default_page = 'front-page'
-    api.content.transition(
-        obj=homepage,
-        to_state='published'
-    )
-
-    setupPortletAt(portal, 'portlets.Events', 'plone.rightcolumn', '/front-page') # noqa
-    setupPortletAt(portal, 'portlets.WichtigesAusDerDPG', 'plone.rightcolumn', '/front-page') # noqa
-    homepage.reindexObject()
-
-    portal['beispiele']['archive'].layout = 'timeline'
-    portal['beispiele']['archive'].reindexObject()
+    # portal['interner-bereich'].manage_setLocalRoles('Authenticated Users', ['Reader', ]) # noqa
 
 
 def uninstall(context):
@@ -452,7 +287,7 @@ def add_catalog_indexes(context, wanted=None):
 
 def setupNavigationPortlet(context, name="", root=None, includeTop=False,
                            currentFolderOnly=False, bottomLevel=0, topLevel=0):
-    from plone.app.portlets.portlets.navigation import Assignment as NavAssignment # noqa
+    from plone.app.portlets.portlets.navigation import Assignment as NavAssignment  # noqa
     target_manager = queryUtility(
         IPortletManager, name='plone.leftcolumn', context=context)
     target_manager_assignments = getMultiAdapter(
@@ -479,27 +314,3 @@ def setupPortletAt(portal, portlet_type, manager, path, name="", **kw):
         name = chooser.chooseName(None, assignment)
 
     mapping[name] = assignment
-
-
-def setupDPGLogin(portal):
-    try:
-        from dpg.login.dpg_login import DPGLoginPlugin
-    except ImportError:
-        pass
-    else:
-        pas = portal.acl_users
-        plugin_name = 'dpglogin'
-        if plugin_name not in pas.objectIds():
-            manager = DPGLoginPlugin(plugin_name, 'DPGLoginPlugin')
-            pas._setObject(plugin_name, manager)
-            provider = pas[plugin_name]
-            provider.manage_activateInterfaces(['IAuthenticationPlugin',
-                                                'IUserEnumerationPlugin',
-                                                'IPropertiesPlugin',
-                                                'IGroupsPlugin',
-                                                'IGroupEnumerationPlugin',
-                                                'IGroupIntrospection'])
-            provider = pas['mutable_properties']
-            provider.manage_activateInterfaces([])
-            api.user.grant_roles(user=api.user.get(username='980146'), roles=['Site Administrator']) # noqa
-            api.user.grant_roles(user=api.user.get(username='153026'), roles=['Site Administrator']) # noqa
