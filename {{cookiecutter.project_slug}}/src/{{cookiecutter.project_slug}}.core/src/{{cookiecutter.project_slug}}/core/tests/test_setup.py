c4d763d410ad360d095f59583806f4f670dfc393
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-04-09 11:09:28 +0200

    Test internal tests as well

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.core/src/{{cookiecutter.project_slug}}/core/tests/test_setup.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.core/src/{{cookiecutter.project_slug}}/core/tests/test_setup.py
@@ -6,6 +6,11 @@ from plone import api
 
 import unittest
 
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
 
 class TestSetup(unittest.TestCase):
     """Test that {{cookiecutter.project_slug}}.core is properly installed."""
@@ -15,19 +20,15 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
         """Test if {{cookiecutter.project_slug}}.core is installed."""
         self.assertTrue(self.installer.isProductInstalled(
             '{{cookiecutter.project_slug}}.core'))
-
-    # def test_plone_app_imagecropping_installed(self):
-    #     self.assertTrue(
-    #         self.installer.isProductInstalled(
-    #             'plone.app.imagecropping'
-    #         )
-    #     )
 
     # def test_plone_restapi_installed(self):
     #     self.assertTrue(
@@ -36,17 +37,12 @@ class TestSetup(unittest.TestCase):
     #         )
     #     )
 
-    # def test_news_item_renamed_to_meldung(self):
-    #     portal_types = getToolByName(self.portal, 'portal_types')
-    #     news_item_fti = getattr(portal_types, 'News Item')
-    #     self.assertEqual('Meldung', news_item_fti.title)
-
     def test_browserlayer(self):
-        """Test that I{{cookiecutter.project_slug.capitalize()}}CoreLayer is registered."""
+        """Test that I{{cookiecutter.project_slug.capitalize()}}CoreLayer is registered.""" # noqa
         from {{cookiecutter.project_slug}}.core.interfaces import (
             I{{cookiecutter.project_slug.capitalize()}}CoreLayer)
         from plone.browserlayer import utils
-        self.assertIn(I{{cookiecutter.project_slug.capitalize()}}CoreLayer, utils.registered_layers())
+        self.assertIn(I{{cookiecutter.project_slug.capitalize()}}CoreLayer, utils.registered_layers()) # noqa
 
 
 class TestUninstall(unittest.TestCase):
@@ -55,16 +51,20 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
-        self.installer.uninstallProducts(['{{cookiecutter.project_slug}}.core'])
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+
+        self.installer.uninstallProducts(['{{cookiecutter.project_slug}}.core']) # noqa
 
     def test_product_uninstalled(self):
-        """Test if {{cookiecutter.project_slug}}.core is cleanly uninstalled."""
+        """Test if {{cookiecutter.project_slug}}.core is cleanly uninstalled.""" # noqa
         self.assertFalse(self.installer.isProductInstalled(
             '{{cookiecutter.project_slug}}.core'))
 
     def test_browserlayer_removed(self):
-        """Test that I{{cookiecutter.project_slug.capitalize()}}CoreLayer is removed."""
-        from {{cookiecutter.project_slug}}.core.interfaces import I{{cookiecutter.project_slug.capitalize()}}CoreLayer
+        """Test that I{{cookiecutter.project_slug.capitalize()}}CoreLayer is removed.""" # noqa
+        from {{cookiecutter.project_slug}}.core.interfaces import I{{cookiecutter.project_slug.capitalize()}}CoreLayer # noqa
         from plone.browserlayer import utils
-        self.assertNotIn(I{{cookiecutter.project_slug.capitalize()}}CoreLayer, utils.registered_layers())
+        self.assertNotIn(I{{cookiecutter.project_slug.capitalize()}}CoreLayer, utils.registered_layers()) # noqa
